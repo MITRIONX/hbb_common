6c4207c87234c0557df6279d3f6ec86ee00dcb2e
timmay1986 <timo@benfilo.de>
2026-02-14 16:48:18 +0100

    feat: configure custom RustDesk server (46.225.113.238)

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["46.225.113.238"];
+pub const RS_PUB_KEY: &str = "NGzFqc4juYHGgJQ0XCQiu2iF2Xj+JJOn86w4LFaEVcM=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
